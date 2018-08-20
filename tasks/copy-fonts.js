10f2b415804a99b606f8d2b1250af1f53cf0b985
Phaj Nướng <baonguyenyam@gmail.com>
2018-08-21 00:13:14 +0700

    add fonts

--- tasks/copy-fonts.js
+++ tasks/copy-fonts.js
@@ -1,8 +1,8 @@
 module.exports = function (gulp, $, browserSync) {
 	gulp.task('copy-fonts', function () {
 		return gulp.src([
-				'./src/fonts/**.*',
+				'./src/webfonts/**.*',
 			])
-			.pipe(gulp.dest('./dist/fonts'));
+			.pipe(gulp.dest('./dist/webfonts'));
 	});
 };
