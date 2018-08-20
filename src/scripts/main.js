5e1da0e5e15a4f6ab9afce745a09ca9c0f6645f7
Phaj Nướng <baonguyenyam@gmail.com>
2018-08-20 23:48:30 +0700

    add block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,20 @@
 // Main
 $(document).ready(function() {
-
+	$('.canhcam-design-1 .select-nav-slider .owl-carousel').owlCarousel({
+		loop: true,
+		padding: 10,
+		margin: 10,
+		nav: true,
+		dots: false,
+		autoHeight: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 3
+			}
+		}
+	})
 });
-
-$('.canhcam-design-1 .slide-sub .owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    dots: false,
-    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        }
-    }
-})
