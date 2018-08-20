57fd8390d0a90b8d26a58b0cbac2288a18960db3
Phaj Nướng <baonguyenyam@gmail.com>
2018-08-21 00:44:17 +0700

    add code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,37 @@
+var baoNguyenApp = {
+	loading: function (i) {
+		i && 1 == i ? $("#loading").removeClass("done").removeClass("finished") : (setTimeout(function () {
+			$("#loading").addClass("done")
+		}, 200), setTimeout(function () {
+			$("#loading").removeClass("done").addClass("finished")
+		}, 1e3))
+	},
+	init: function() {
+		// Bật Loading
+		this.loading(true)
+		$('[data-toggle="tooltip"]').tooltip()
+		$('.canhcam-design-1 .select-nav-slider .owl-carousel').owlCarousel({
+			loop: true,
+			padding: 10,
+			margin: 10,
+			nav: true,
+			dots: false,
+			autoHeight: true,
+			navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 3
+				}
+			}
+		})
+		// Tắt Loading
+		this.loading(false)
+	}
+}
 // Main
 $(document).ready(function() {
-	$('.canhcam-design-1 .select-nav-slider .owl-carousel').owlCarousel({
-		loop: true,
-		padding: 10,
-		margin: 10,
-		nav: true,
-		dots: false,
-		autoHeight: true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 3
-			}
-		}
-	})
+	baoNguyenApp.init()
 });
