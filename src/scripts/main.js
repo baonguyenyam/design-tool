fc162761493036ecc23566df1f8f302d3cf7c0b3
DESKTOP-MLTTJ1T\ADMIN <dinhlong241@gmail.com>
2018-08-20 16:55:21 +0700

    update template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,20 @@
 // Main
-$(document).ready(function () {
-	
+$(document).ready(function() {
+
 });
+
+$('.canhcam-design-1 .slide-sub .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        }
+    }
+})
